69ec1ca7e10eb2fcd8296850a4e1e2467b3461a9
CHIRAG <freeautomationlearning@gmail.com>
2017-05-21 14:50:32 +0530

    Test java edit

--- src/com/automationlearning/TestJava.java
+++ src/com/automationlearning/TestJava.java
@@ -2,6 +2,11 @@ package com.automationlearning;
 
 public class TestJava {
 
+	/*
+	 * This is a Test Script
+	 * I will provide all code shortly
+	 * 
+	 */
 	public static void main(String[] args) {
 		System.out.println("Appium");
 
